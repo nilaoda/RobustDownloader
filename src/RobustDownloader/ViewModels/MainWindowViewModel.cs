ad677592f3f48bd83df4afae1ddf50fe95516677
nilaoda <nilaoda@live.com>
2026-07-10 01:16:54 +0800

    Bump version to 0.0.6

--- src/RobustDownloader/ViewModels/MainWindowViewModel.cs
+++ src/RobustDownloader/ViewModels/MainWindowViewModel.cs
@@ -1086,7 +1086,7 @@ public partial class MainWindowViewModel : ViewModelBase
             var version = typeof(MainWindowViewModel).Assembly
                 .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
                 ?.InformationalVersion;
-            return string.IsNullOrWhiteSpace(version) ? "0.0.4" : version.Split('+')[0];
+            return string.IsNullOrWhiteSpace(version) ? "unknown" : version.Split('+')[0];
         }
     }
 
